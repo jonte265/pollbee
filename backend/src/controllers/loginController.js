70541a0dc2e3add8c4151aadf32691d6ea13f27d
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-08-22 14:42:42 +0200

    add login working

--- backend/src/controllers/loginController.js
+++ backend/src/controllers/loginController.js
@@ -1,5 +1,29 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import supabase from '../config/supabaseClient.js';
+
 export const loginUser = async (req, res) => {
   const { username, password } = req.body;
 
-  res.json({ message: 'login here' });
+  const { data, error } = await supabase
+    .from('users')
+    .select('*')
+    .eq('username', username)
+    .single();
+
+  console.log(data);
+
+  if (!data) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, data.password);
+
+  if (!isPasswordCorrect) {
+    return res.status(401).json({ message: 'Wrong credentials' });
+  }
+
+  res.status(200).json({ message: `User ${username} logged in!` });
 };
+
+// fix capatilized
