2b4d40392d18808fbc14963c87a4cdd738584c14
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-08-25 01:43:37 +0200

    add custom css variables theme

--- frontend/components/LoginForm.tsx
+++ frontend/components/LoginForm.tsx
@@ -6,14 +6,14 @@ function LoginForm() {
       <input
         type='text'
         placeholder='Username'
-        className='rounded-4xl p-2 text-center'
+        className='rounded-4xl p-2 text-center bg-text-50'
       />
       <input
         type='password'
         placeholder='Password'
-        className='rounded-4xl p-2 text-center'
+        className='rounded-4xl p-2 text-center bg-text-50'
       />
-      <button type='submit' className='bg-amber-100 rounded-4xl p-4'>
+      <button type='submit' className='bg-secondary rounded-4xl p-4'>
         Login
       </button>
     </form>
