a80373f66eac5249f83509daac37d7124c5c86de
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-08-25 00:35:59 +0200

    create header component and login

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
 function Header() {
   return (
-    <header>
-      <h1 className='font-bold text-2xl'>PollBee 🐝</h1>
+    <header className='flex justify-between items-center pb-4'>
+      <Link href='/'>
+        <h1 className='font-bold text-2xl'>PollBee 🐝</h1>
+      </Link>
+      <Link href='/login'>
+        <p className='font-bold'>Login</p>
+      </Link>
     </header>
   );
 }
