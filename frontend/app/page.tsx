7c15c0ab25816f21972b7d34a690b2d917c270c8
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-08-26 12:50:24 +0200

    small adjustment gap margin

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,13 +5,13 @@ import FeatureSection from '@/components/FeatureSection';
 export default function Home() {
   return (
     <>
-      <main className='flex flex-col justify-center items-center gap-8 max-w-xl mx-auto p-6 text-center'>
+      <main className='flex flex-col justify-center items-center gap-4 max-w-xl mx-auto p-6 text-center'>
         <h1 className='text-4xl font-extrabold mb-4'>PollBee 🐝</h1>
         <p className='text-lg text-gray-700'>
           Create and share live polls easily. Build your profile to manage all
           your polls in one place.
         </p>
-        <div className='flex gap-4 mt-6'>
+        <div className='flex gap-4 mb-6'>
           <Link href='/signup'>
             <button className='px-6 py-2 border border-primary text-primary rounded-3xl font-semibold hover:bg-primary hover:text-background transition'>
               Sign Up
