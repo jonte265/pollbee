81f9a64ae7a01f87e38723f6a23f2df0b67a9dfc
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-08-26 09:30:31 +0200

    add homepage text and signup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from 'next/image';
+import Link from 'next/link';
+import PrimaryBtn from '@/components/PrimaryBtn';
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello world</h1>
+    <main className='flex flex-col justify-center items-center gap-8 max-w-xl mx-auto p-6 text-center'>
+      <h1 className='text-4xl font-extrabold mb-4'>PollBee 🐝</h1>
+      <p className='text-lg text-gray-700'>
+        Create and share live polls easily. Build your profile to manage all
+        your polls in one place.
+      </p>
+      <div className='flex gap-4 mt-6'>
+        <Link href='/signup'>
+          <button className='px-6 py-2 border border-primary text-primary rounded-3xl font-semibold hover:bg-primary hover:text-background transition'>
+            Sign Up
+          </button>
+        </Link>
+      </div>
     </main>
   );
 }
