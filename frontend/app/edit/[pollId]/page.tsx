f03da128b744f2daf7fa9d42bdc89324b3b0c0f3
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-08-26 21:00:50 +0200

    enable edit state for field

--- frontend/app/edit/[pollId]/page.tsx
+++ frontend/app/edit/[pollId]/page.tsx
@@ -5,6 +5,7 @@ import { useState, use, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import LoadingSpin from '@/components/LoadingSpin';
 import { FaCheck, FaTimes } from 'react-icons/fa';
+import PrimaryBtn from '@/components/PrimaryBtn';
 
 type EditPollParams = Promise<{
   pollId: string;
@@ -12,6 +13,12 @@ type EditPollParams = Promise<{
 
 type PollDataType = {
   poll_title: string;
+  poll_options: PollOptionType[];
+};
+
+type PollOptionType = {
+  option_text: string;
+  id: number;
 };
 
 function EditPoll({ params }: { params: EditPollParams }) {
@@ -21,6 +28,12 @@ function EditPoll({ params }: { params: EditPollParams }) {
   const router = useRouter();
 
   const { pollId } = use(params);
+
+  const [editMode, setEditMode] = useState(-10);
+
+  function enableEditMode(id: number) {
+    setEditMode(id);
+  }
 
   async function fetchPollData() {
     try {
@@ -41,16 +54,45 @@ function EditPoll({ params }: { params: EditPollParams }) {
   return (
     <main className='flex flex-col gap-8 items-center justify-center'>
       <h2 className='text-xl'>Edit Poll</h2>
+
       {pollData ? (
-        <div>
-          <p>Title:</p>
-          <p>{pollData.poll_title}</p>
-          <p>Options:</p>
-          <p>{pollData.poll_options[0].option_text}</p>
+        <div className='flex flex-col'>
+          <p className='text-xl'>Title:</p>
+          <div className='flex justify-center items-center gap-4'>
+            <p>{pollData.poll_title}</p>
+            <button className='font-bold px-4 py-2 bg-primary-50 hover:bg-primary-100 hover:underline rounded-4xl  transition-all ease-in-out'>
+              Edit
+            </button>
+          </div>
+
+          <p className='text-xl mt-4'>Options:</p>
+          <div className='flex flex-col gap-2'>
+            {pollData.poll_options.map((opt, index) =>
+              editMode === opt.id ? (
+                <p key={opt.id}>Edit mode on</p>
+              ) : (
+                <div
+                  key={opt.id}
+                  className='flex justify-center items-center gap-4'
+                >
+                  <p>{opt.option_text}</p>
+                  <button
+                    onClick={() => setEditMode(opt.id)}
+                    className='font-bold px-4 py-2 bg-primary-50 hover:bg-primary-100 hover:underline rounded-4xl  transition-all ease-in-out'
+                  >
+                    Edit
+                  </button>
+                </div>
+              )
+            )}
+          </div>
         </div>
       ) : (
         <LoadingSpin />
       )}
+      <Link href='/profile'>
+        <PrimaryBtn btnText='Done' />
+      </Link>
     </main>
   );
 }
