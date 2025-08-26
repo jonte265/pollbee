934cb33f4a4e5e1b56f5f3d87fd003ab011eccb4
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-08-26 20:34:43 +0200

    add fetch edit page

--- frontend/app/edit/[pollId]/page.tsx
+++ frontend/app/edit/[pollId]/page.tsx
@@ -10,16 +10,47 @@ type EditPollParams = Promise<{
   pollId: string;
 }>;
 
+type PollDataType = {
+  poll_title: string;
+};
+
 function EditPoll({ params }: { params: EditPollParams }) {
+  const [pollData, setPollData] = useState<PollDataType | null>(null);
+
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
   const router = useRouter();
 
   const { pollId } = use(params);
 
-  console.log(pollId);
+  async function fetchPollData() {
+    try {
+      const res = await fetch(`${apiUrl}/polls/${pollId}`);
+      const data = await res.json();
+
+      console.log(data);
+      setPollData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchPollData();
+  }, [pollId]);
 
   return (
     <main className='flex flex-col gap-8 items-center justify-center'>
-      <h1>edit</h1>
+      <h2 className='text-xl'>Edit Poll</h2>
+      {pollData ? (
+        <div>
+          <p>Title:</p>
+          <p>{pollData.poll_title}</p>
+          <p>Options:</p>
+          <p>{pollData.poll_options[0].option_text}</p>
+        </div>
+      ) : (
+        <LoadingSpin />
+      )}
     </main>
   );
 }
