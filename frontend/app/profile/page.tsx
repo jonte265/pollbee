8585db347aa691ad71402ce283c6cd05a11f558e
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-08-27 23:28:29 +0200

    add delete account function

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -18,28 +18,55 @@ type profileDataType = {
 function ProfilePage() {
   // Redirect if not logged in
   const router = useRouter();
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
-  function deleteAccount() {}
+  const [userNameLocal, setUserNameLocal] = useState<string | null>(null);
+  const [token, setToken] = useState<string | null>(null); // Get jwt token localstorage
 
-  const userNameLocal = localStorage.getItem('username');
+  const [askDelete, setAskDelete] = useState(false);
 
   useEffect(() => {
-    const tokenLocal = localStorage.getItem('token');
+    const username = localStorage.getItem('username');
+    const tok = localStorage.getItem('token');
 
-    if (tokenLocal === null) {
+    setUserNameLocal(username);
+    setToken(tok);
+
+    if (!tok) {
       router.push('/login');
-    } else {
     }
   }, []);
+
+  async function deleteAccount(userDelete: string) {
+    console.log(userDelete);
+
+    const res = await fetch(`${apiUrl}/users`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        username: userDelete,
+      }),
+    });
+
+    if (!res.ok) {
+      console.log('Error, not ok fail delete account');
+    }
+
+    const data = await res.json();
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    // router.push('/');
+    window.location.href = '/'; // Refresh window
+  }
 
   const [profileData, setProfileData] = useState<profileDataType[]>([]);
 
   const [loading, setLoading] = useState(false);
 
   async function fetchProfileData() {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-    const token = localStorage.getItem('token'); // Get jwt token localstorage
-
     setLoading(true);
 
     const res = await fetch(`${apiUrl}/profile`, {
@@ -59,8 +86,10 @@ function ProfilePage() {
   }
 
   useEffect(() => {
-    fetchProfileData();
-  }, []);
+    if (token) {
+      fetchProfileData();
+    }
+  }, [token]);
 
   return (
     <main className='flex flex-col justify-center items-center gap-8'>
@@ -74,7 +103,7 @@ function ProfilePage() {
       </Link>
 
       {loading && <LoadingSpin />}
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-32'>
         {profileData.length > 0 &&
           profileData.map((poll, index) => (
             <PollCard
@@ -86,11 +115,38 @@ function ProfilePage() {
             />
           ))}
       </div>
-      <button
-        className={`flex justify-center items-center gap-2 bg-red-500 text-background font-bold rounded-4xl px-4 py-2 hover:bg-red-700 transition-all ease-in-out`}
-      >
-        <FaExclamationTriangle /> Delete Account
-      </button>
+      {userNameLocal && askDelete === false && (
+        <button
+          onClick={() => setAskDelete(true)}
+          className={`flex justify-center items-center gap-2 bg-red-500 text-background font-bold rounded-4xl px-4 py-2 hover:bg-red-700 transition-all ease-in-out`}
+        >
+          <FaExclamationTriangle /> Delete Account
+        </button>
+      )}
+      {askDelete && (
+        <div>
+          <p className='text-center'>
+            Are you sure you want to delete your account? <br />
+            This will permanently delete all your data
+          </p>
+          <div className='flex flex-row gap-2 pt-4'>
+            <button
+              onClick={() => {
+                if (userNameLocal) deleteAccount(userNameLocal);
+              }}
+              className={`flex justify-center items-center gap-2 bg-red-500 text-background font-bold rounded-4xl px-4 py-2 hover:bg-red-700 transition-all ease-in-out`}
+            >
+              Yes, delete account.
+            </button>
+            <button
+              onClick={() => setAskDelete(false)}
+              className={`flex justify-center items-center gap-2 bg-text text-background font-bold rounded-4xl px-4 py-2 hover:bg-text-700 transition-all ease-in-out`}
+            >
+              No, keep my account.
+            </button>
+          </div>
+        </div>
+      )}
     </main>
   );
 }
