94d2140441b7bb1a4bd7b0b5fec470cd1993348f
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-08-30 19:37:55 +0200

    add delete poll

--- frontend/app/edit/[pollId]/page.tsx
+++ frontend/app/edit/[pollId]/page.tsx
@@ -7,6 +7,7 @@ import LoadingSpin from '@/components/LoadingSpin';
 import { FaCheck, FaTimes } from 'react-icons/fa';
 import PrimaryBtn from '@/components/PrimaryBtn';
 import { motion } from 'motion/react';
+import { FaExclamationTriangle } from 'react-icons/fa';
 
 type EditPollParams = Promise<{
   pollId: string;
@@ -15,6 +16,7 @@ type EditPollParams = Promise<{
 type PollDataType = {
   poll_title: string;
   id: number;
+  message: string;
   poll_options: PollOptionType[];
 };
 
@@ -41,8 +43,45 @@ function EditPoll({ params }: { params: EditPollParams }) {
 
   const [editMode, setEditMode] = useState(-10);
   const [updateText, setUpdateText] = useState('');
+  const [askDelete, setAskDelete] = useState(false);
+  const [token, setToken] = useState<string | null>(null); // Get jwt token localstorage
 
   const [loadingState, setLoadingState] = useState(false);
+
+  useEffect(() => {
+    const username = localStorage.getItem('username');
+    const tok = localStorage.getItem('token');
+
+    setToken(tok);
+
+    if (!tok) {
+      router.push('/login');
+    }
+  }, []);
+
+  async function deletePoll(pollId: number) {
+    console.log(pollId);
+
+    const res = await fetch(`${apiUrl}/polls`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        pollid: pollId,
+      }),
+    });
+
+    if (!res.ok) {
+      console.log('Error, not ok fail delete poll');
+    }
+
+    const data = await res.json();
+
+    router.push('/profile');
+    // window.location.href = '/profile'; // Refresh window
+  }
 
   async function saveChange(
     updateText: string,
@@ -109,7 +148,7 @@ function EditPoll({ params }: { params: EditPollParams }) {
       const res = await fetch(`${apiUrl}/polls/${pollId}`);
       const data = await res.json();
 
-      console.log(data);
+      // console.log(data);
       setPollData(data);
     } catch (error) {
       console.error(error);
@@ -126,111 +165,148 @@ function EditPoll({ params }: { params: EditPollParams }) {
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <main className='flex flex-col gap-8 items-center justify-center'>
-        <h2 className='text-xl text-center font-bold'>Edit Poll</h2>
+      {pollData?.message === 'Poll not found' ? (
+        <p className='text-center text-2xl'>Poll not found</p>
+      ) : (
+        <main className='flex flex-col gap-8 items-center justify-center'>
+          <h2 className='text-xl text-center font-bold'>Edit Poll</h2>
 
-        {pollData ? (
-          <div className='flex flex-col max-w-sm w-full px-4'>
-            <p className='mb-1'>Title:</p>
+          {pollData ? (
+            <div className='flex flex-col max-w-sm w-full px-4'>
+              <p className='mb-1'>Title:</p>
 
-            <div className='border border-gray-300 rounded-4xl px-4 py-2 flex flex-col gap-2'>
-              {editMode === -5 ? (
-                <div className='flex gap-2'>
-                  <input
-                    disabled={loadingState}
-                    value={updateText}
-                    onChange={(e) => setUpdateText(e.target.value)}
-                    type='text'
-                    placeholder={pollData.poll_title}
-                    className='flex-grow rounded-4xl p-2 pl-4 bg-primary-50'
-                  />
-                  {loadingState ? (
-                    <LoadingSpin />
-                  ) : (
-                    <div className='flex items-center justify-center gap-4'>
-                      <button
-                        onClick={() => saveChange(updateText, 'forTitle')}
-                      >
-                        <FaCheck />
-                      </button>
-                      <button onClick={cancelEdit}>
-                        <FaTimes />
-                      </button>
-                    </div>
-                  )}
-                </div>
-              ) : (
-                <div className='flex justify-between items-center gap-4'>
-                  <p className='font-semibold'>{pollData.poll_title}</p>
-                  <button
-                    onClick={() => setEditMode(-5)}
-                    className='font-semibold px-4 py-2 bg-primary-50 hover:bg-primary-100 hover:underline rounded-4xl  transition-all ease-in-out'
-                  >
-                    Edit
-                  </button>
-                </div>
-              )}
-            </div>
-
-            <p className='mt-4 mb-1'>Options:</p>
-            <div className='flex flex-col gap-2'>
-              {[...pollData.poll_options]
-                .sort((a, b) => a.id - b.id)
-                .map((opt, index) =>
-                  editMode === opt.id ? (
-                    <div
-                      className='border border-gray-300 rounded-4xl px-4 py-2 flex flex-row gap-2'
-                      key={opt.id}
+              <div className='border border-gray-300 rounded-4xl px-4 py-2 flex flex-col gap-2'>
+                {editMode === -5 ? (
+                  <div className='flex gap-2'>
+                    <input
+                      disabled={loadingState}
+                      value={updateText}
+                      onChange={(e) => setUpdateText(e.target.value)}
+                      type='text'
+                      placeholder={pollData.poll_title}
+                      className='flex-grow rounded-4xl p-2 pl-4 bg-primary-50'
+                    />
+                    {loadingState ? (
+                      <LoadingSpin />
+                    ) : (
+                      <div className='flex items-center justify-center gap-4'>
+                        <button
+                          onClick={() => saveChange(updateText, 'forTitle')}
+                        >
+                          <FaCheck />
+                        </button>
+                        <button onClick={cancelEdit}>
+                          <FaTimes />
+                        </button>
+                      </div>
+                    )}
+                  </div>
+                ) : (
+                  <div className='flex justify-between items-center gap-4'>
+                    <p className='font-semibold'>{pollData.poll_title}</p>
+                    <button
+                      onClick={() => setEditMode(-5)}
+                      className='font-semibold px-4 py-2 bg-primary-50 hover:bg-primary-100 hover:underline rounded-4xl  transition-all ease-in-out'
                     >
-                      <input
-                        disabled={loadingState}
-                        value={updateText}
-                        onChange={(e) => setUpdateText(e.target.value)}
-                        type='text'
-                        placeholder={opt.option_text}
-                        className='flex-grow rounded-4xl p-2 pl-4 bg-primary-50'
-                      />
-                      {loadingState ? (
-                        <LoadingSpin />
-                      ) : (
-                        <div className='flex items-center justify-center gap-4'>
-                          <button
-                            onClick={() =>
-                              saveChange(updateText, 'forOption', opt.id)
-                            }
-                          >
-                            <FaCheck />
-                          </button>
-                          <button onClick={cancelEdit}>
-                            <FaTimes />
-                          </button>
-                        </div>
-                      )}
-                    </div>
-                  ) : (
-                    <div
-                      key={opt.id}
-                      className='border border-gray-300 rounded-4xl px-4 py-2 flex justify-between items-center gap-4'
-                    >
-                      <p className='font-semibold'>{opt.option_text}</p>
-                      <button
-                        onClick={() => setEditMode(opt.id)}
-                        className='font-semibold px-4 py-2 bg-primary-50 hover:bg-primary-100 hover:underline rounded-4xl  transition-all ease-in-out'
-                      >
-                        Edit
-                      </button>
-                    </div>
-                  )
+                      Edit
+                    </button>
+                  </div>
                 )}
+              </div>
+
+              <p className='mt-4 mb-1'>Options:</p>
+              <div className='flex flex-col gap-2'>
+                {[...pollData.poll_options]
+                  .sort((a, b) => a.id - b.id)
+                  .map((opt, index) =>
+                    editMode === opt.id ? (
+                      <div
+                        className='border border-gray-300 rounded-4xl px-4 py-2 flex flex-row gap-2'
+                        key={opt.id}
+                      >
+                        <input
+                          disabled={loadingState}
+                          value={updateText}
+                          onChange={(e) => setUpdateText(e.target.value)}
+                          type='text'
+                          placeholder={opt.option_text}
+                          className='flex-grow rounded-4xl p-2 pl-4 bg-primary-50'
+                        />
+                        {loadingState ? (
+                          <LoadingSpin />
+                        ) : (
+                          <div className='flex items-center justify-center gap-4'>
+                            <button
+                              onClick={() =>
+                                saveChange(updateText, 'forOption', opt.id)
+                              }
+                            >
+                              <FaCheck />
+                            </button>
+                            <button onClick={cancelEdit}>
+                              <FaTimes />
+                            </button>
+                          </div>
+                        )}
+                      </div>
+                    ) : (
+                      <div
+                        key={opt.id}
+                        className='border border-gray-300 rounded-4xl px-4 py-2 flex justify-between items-center gap-4'
+                      >
+                        <p className='font-semibold'>{opt.option_text}</p>
+                        <button
+                          onClick={() => setEditMode(opt.id)}
+                          className='font-semibold px-4 py-2 bg-primary-50 hover:bg-primary-100 hover:underline rounded-4xl  transition-all ease-in-out'
+                        >
+                          Edit
+                        </button>
+                      </div>
+                    )
+                  )}
+              </div>
             </div>
-          </div>
-        ) : (
-          <LoadingSpin />
-        )}
-        <Link href='/profile'>
-          <PrimaryBtn btnText='Done' />
-        </Link>
-      </main>
+          ) : (
+            <LoadingSpin />
+          )}
+          <Link href='/profile'>
+            <PrimaryBtn btnText='Done' />
+          </Link>
+
+          {askDelete === false && (
+            <button
+              onClick={() => setAskDelete(true)}
+              className={`flex justify-center items-center gap-2 bg-red-500 text-background font-bold rounded-4xl px-4 py-2 hover:bg-red-700 transition-all ease-in-out`}
+            >
+              <FaExclamationTriangle /> Delete Poll
+            </button>
+          )}
+          {askDelete && (
+            <div>
+              <p className='text-center'>
+                Are you sure you want to delete this poll? <br />
+                This will permanently delete the poll and its data.
+              </p>
+              <div className='flex flex-row gap-2 pt-4'>
+                <button
+                  onClick={() => {
+                    if (pollData?.id) deletePoll(pollData?.id);
+                  }}
+                  className={`flex justify-center items-center gap-2 bg-red-500 text-background font-bold rounded-4xl px-4 py-2 hover:bg-red-700 transition-all ease-in-out`}
+                >
+                  Yes, delete poll
+                </button>
+                <button
+                  onClick={() => setAskDelete(false)}
+                  className={`flex justify-center items-center gap-2 bg-text text-background font-bold rounded-4xl px-4 py-2 hover:bg-text-700 transition-all ease-in-out`}
+                >
+                  No, keep the poll
+                </button>
+              </div>
+            </div>
+          )}
+        </main>
+      )}
     </motion.div>
   );
 }
