b0145933952a3829abeb6e27d53f95b52517e89a
jonte265 <200072657+jonte265@users.noreply.github.com>
2026-04-29 21:59:36 +0200

    featuresection tweak

--- frontend/components/FeatureSection.tsx
+++ frontend/components/FeatureSection.tsx
@@ -17,24 +17,24 @@ function FeatureSection({
   img,
 }: FeatureSectionTypes) {
   return (
-    <section className="flex justify-center items-center p-16 rounded-4xl bg-gray-100">
-      <div className="flex flex-col justify-center text-center items-center gap-8">
+    <section className="flex flex-col justify-center items-center gap-8 p-8 sm:p-16 rounded-4xl bg-gray-100">
+      <div className="flex flex-col gap-4">
         <H2>{sectionTitle}</H2>
         <ul className="flex flex-col justify-center items-center">
           {li1 && <li>{li1}</li>}
           {li2 && <li>{li2}</li>}
           {li3 && <li>{li3}</li>}
         </ul>
-        {img && (
-          <Image
-            className="rounded-4xl"
-            src={`/${img}`}
-            width={500}
-            height={500}
-            alt="Poll"
-          />
-        )}
       </div>
+      {img && (
+        <Image
+          className="rounded-4xl"
+          src={`/${img}`}
+          width={600}
+          height={600}
+          alt="Poll"
+        />
+      )}
     </section>
   );
 }
