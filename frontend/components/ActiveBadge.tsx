c37055135910c55959cb17a1c9f2d9cfda89c106
jonte265 <200072657+jonte265@users.noreply.github.com>
2026-05-01 13:53:37 +0200

    fix activebadge

--- frontend/components/ActiveBadge.tsx
+++ frontend/components/ActiveBadge.tsx
@@ -1,20 +1,32 @@
+import { LuCirclePause } from "react-icons/lu";
+import Typography from "./ui/typography/Typography";
+
 type activeBadgeTypes = {
   isActive: boolean;
 };
 
 export default function ActiveBadge({ isActive }: activeBadgeTypes) {
   return (
-    <div>
-      <p
-        className={`py-1 px-2 rounded-4xl text-sm font-bold flex justify-center items-center gap-2 ${
-          isActive ? "bg-badge-active" : "bg-badge-disabled"
-        }`}
-      >
-        {isActive && (
-          <span className="h-2 w-2 rounded-full bg-green-600 animate-pulse" />
-        )}
-        {isActive ? "Active" : "Inactive"}
-      </p>
-    </div>
+    <>
+      {isActive ? (
+        <div
+          className={`py-1 px-2 rounded-4xl flex justify-center items-center gap-1 bg-accent/20 `}
+        >
+          <span className="h-2 w-2 rounded-full bg-primary-600 animate-pulse" />
+          <Typography small bold>
+            Active
+          </Typography>
+        </div>
+      ) : (
+        <div
+          className={`py-1 px-2 rounded-4xl flex justify-center items-center gap-1 bg-secondary/20 `}
+        >
+          <LuCirclePause className="text-secondary-600" size={14} />
+          <Typography bold small light>
+            Inactive
+          </Typography>
+        </div>
+      )}
+    </>
   );
 }
