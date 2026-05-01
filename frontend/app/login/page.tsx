bb4ae134393ce982e16c59ab6ef7df2f06aaefd9
jonte265 <200072657+jonte265@users.noreply.github.com>
2026-05-01 15:34:24 +0200

    btn fix

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -91,14 +91,14 @@ function LoginPage() {
           {!loading ? (
             <button
               type="submit"
-              className="bg-primary text-button-text-white font-bold rounded-4xl px-4 py-2 hover:bg-primary-700 transition-all ease-in-out"
+              className="bg-primary text-background font-bold rounded-4xl px-4 py-2 hover:bg-primary-700 transition-all ease-in-out"
             >
               Login
             </button>
           ) : (
             <button
               disabled
-              className="bg-primary-100 text-button-text-white font-bold rounded-4xl px-4 py-2 transition-all ease-in-out"
+              className="bg-primary-100 text-background font-bold rounded-4xl px-4 py-2 transition-all ease-in-out"
             >
               Login
             </button>
