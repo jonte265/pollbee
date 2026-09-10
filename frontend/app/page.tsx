a6af8e29d100fedf0f1d8097d1fe8d5aa48f6055
jonte265 <200072657+jonte265@users.noreply.github.com>
2026-09-10 14:52:59 +0200

    comments

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -22,78 +22,78 @@ export default function Home() {
   }, []);
 
   return (
-    <>
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <main className="flex flex-col gap-16 text-center max-w-6xl m-auto">
-          <div className="flex flex-col gap-8 pb-16">
-            <div className="flex flex-col gap-2 text-center ">
-              <HeroEmoji />
-              <H2 large>Create and share live polls easily</H2>
-              <Typography>
-                Build your profile to manage all your polls in one place
-              </Typography>
-            </div>
-            <div className="flex justify-center items-center">
-              {signedIn ? (
-                <Link href="/profile">
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <main className="flex flex-col gap-16 text-center max-w-6xl m-auto">
+        <div className="flex flex-col items-center gap-8">
+          <div className="flex flex-col gap-2 text-center ">
+            <HeroEmoji />
+            <H2 large>Create and share live polls easily</H2>
+            <Typography>
+              Build your profile to manage all your polls in one place
+            </Typography>
+          </div>
+          {/* Cta Buttons */}
+          <div className="flex justify-center items-center">
+            {signedIn ? (
+              <Link href="/profile">
+                <Button
+                  btnText={
+                    <>
+                      <LuCircleUserRound /> Profile
+                    </>
+                  }
+                />
+              </Link>
+            ) : (
+              <div className="flex flex-row gap-2">
+                <Link href="/poll/iI89geUz">
+                  <Button variant="outline" btnText={<>Try sample poll</>} />
+                </Link>
+                <Link href="/signup">
                   <Button
                     btnText={
                       <>
-                        <LuCircleUserRound /> Profile
+                        Sign up <LuArrowRight />
                       </>
                     }
                   />
                 </Link>
-              ) : (
-                <div className="flex flex-row gap-2">
-                  <Link href="/poll/iI89geUz">
-                    <Button variant="outline" btnText={<>Try sample poll</>} />
-                  </Link>
-                  <Link href="/signup">
-                    <Button
-                      btnText={
-                        <>
-                          Sign up <LuArrowRight />
-                        </>
-                      }
-                    />
-                  </Link>
-                </div>
-              )}
-            </div>
+              </div>
+            )}
           </div>
+        </div>
 
-          <div className="flex flex-col gap-4">
-            <FeatureSection
-              sectionTitle={"Create Polls Instantly with AI Assistance 📊"}
-              li1={"Set up a poll in seconds with a simple interface"}
-              li2="Add unlimited options and control vote settings"
-              li3="Generate poll ideas automatically with AI"
-              img="create-poll-page.png"
-            />
+        {/* Feature cards */}
+        <div className="flex flex-col gap-8">
+          <FeatureSection
+            sectionTitle={"Create Polls Instantly with AI Assistance 📊"}
+            li1={"Set up a poll in seconds with a simple interface"}
+            li2="Add unlimited options and control vote settings"
+            li3="Generate poll ideas automatically with AI"
+            img="create-poll-page.png"
+          />
 
-            <FeatureSection
-              sectionTitle="Share Seamlessly 🔁"
-              li1="Generate a shareable link and share with anyone"
-              li2="No login required for voters"
-              li3="Real-time updates as votes come in"
-              img="vote-poll-page.png"
-            />
+          <FeatureSection
+            sectionTitle="Share Seamlessly 🔁"
+            li1="Generate a shareable link and share with anyone"
+            li2="No login required for voters"
+            li3="Real-time updates as votes come in"
+            img="vote-poll-page.png"
+          />
 
-            <FeatureSection
-              sectionTitle="Track Results Live 📈"
-              li1="See live vote counts"
-              li2="Get insights on poll engagement"
-              li3="More built-in analytics coming soon"
-              img="poll-analytics-page.png"
-            />
-          </div>
-        </main>
-      </motion.div>
-    </>
+          <FeatureSection
+            sectionTitle="Track Results Live 📈"
+            li1="See live vote counts"
+            li2="Get insights on poll engagement"
+            li3="More built-in analytics coming soon"
+            img="poll-analytics-page.png"
+          />
+        </div>
+      </main>
+    </motion.div>
   );
 }
